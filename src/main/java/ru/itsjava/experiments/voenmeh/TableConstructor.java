7de39b154742fd772df4b6c269d3fb8c965a7d51
Pavel <savin.pavel.a@gmail.com>
2023-05-04 19:41:59 +0400

    tableConstructor update

--- src/main/java/ru/itsjava/experiments/voenmeh/TableConstructor.java
+++ src/main/java/ru/itsjava/experiments/voenmeh/TableConstructor.java
@@ -12,18 +12,33 @@ public class TableConstructor {
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String input;
             while ((input = reader.readLine()) != null) {
-//                // ENGLISH version
+//                // ENGLISH version EDITORS
 //                String first = input.split(",", 2)[0];
 //                String second = input.split(",", 2)[1];
 //                System.out.println("<tr><td><strong>" + first + "</strong></td><td>" + second.trim() + "</td></tr>");
 
-                // RUSSIAN version
-                String first = input.split(" ", 4)[0];
-                String second = input.split(" ", 4)[1];
-                String third = input.split(" ", 4)[2];
-                String text = input.split(" ", 4)[3].trim();
+//                // RUSSIAN version редколлегия
+//                String first = input.split(" ", 4)[0];
+//                String second = input.split(" ", 4)[1];
+//                String third = input.split(" ", 4)[2];
+//                String text = input.split(" ", 4)[3].trim();
+//
+//                System.out.println("<tr><td><strong>" + first + " " + second + " " + third + "</strong></td><td>" + text + "</td></tr>");
 
-                System.out.println("<tr><td><strong>" + first + " " + second + " " + third + "</strong></td><td>" + text + "</td></tr>");
+//                //RU ver -- редакционный совет
+//                String first = input.split(" ", 4)[0];
+//                String second = input.split(" ", 4)[1];
+//                String third = input.split(" ", 4)[2];
+//                String text = input.split(" ", 4)[3].trim();
+//
+//                System.out.println("<tr><td><strong>" + first + " " + second + " " + third + "</strong></td><td>" + text + "</td></tr>");
+
+                //EN ver -- Editorial Board
+                String first = input.split(" ", 3)[0];
+                String second = input.split(" ", 3)[1];
+                String text = input.split(" ", 3)[2].trim();
+
+                System.out.println("<tr><td><strong>" + first + " " + second + "</strong></td><td>" + text + "</td></tr>");
 
             }
         } catch (IOException e) {
